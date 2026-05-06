ee3e7f9f7c37d1cd82b37d4c3baffb14c4172600
ShiralynDev <jultomtenriktig@gmail.com>
2026-05-06 21:20:01 +0200

    allow "" in command parameters

--- Server/Command/Common/CommandSystemHelperMethods.cs
+++ Server/Command/Common/CommandSystemHelperMethods.cs
@@ -1,4 +1,7 @@
-﻿namespace Server.Command.Common
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Server.Command.Common
 {
     public class CommandSystemHelperMethods
     {
@@ -15,7 +18,11 @@
         public static void SplitCommandParamArray(string command, out string[] parameters)
         {
             parameters = null;
-            var paramArray = command.Split(' ');
+            var paramArray = Regex.Matches(command, "\"[^\"]+\"|[^ \"]+")
+            .Cast<Match>()
+            .Select(m => m.Value.Trim('"'))
+            .ToArray();
+
             if (paramArray.Length > 0)
                 if (!string.IsNullOrEmpty(paramArray[0]))
                     parameters = paramArray;
