b22b1a40447ce880b18fa5ed2ff7390107c32530
Drew Banyai <DrewBanyai@gmail.com>
2026-05-07 22:50:47 -0400

    Updating NetPeerConfiguration Send and Receive buffer size to 4 MB from 1.5 MB and increasing Default Outgoing Message Capacity to 500 KB

--- Server/Context/ServerContext.cs
+++ Server/Context/ServerContext.cs
@@ -33,9 +33,9 @@ namespace Server.Context
         // Configuration object
         public static NetPeerConfiguration Config { get; } = new NetPeerConfiguration("LMP")
         {
-            SendBufferSize = 1500000, //~1.43 MB
-            ReceiveBufferSize = 1500000, //~1.43 MB
-            DefaultOutgoingMessageCapacity = 500000, //~488 KB
+            SendBufferSize = 4194304, //4 MB
+            ReceiveBufferSize = 4194304, //4 MB
+            DefaultOutgoingMessageCapacity = 512000, //~500 KB
             SuppressUnreliableUnorderedAcks = true,
         };
 
